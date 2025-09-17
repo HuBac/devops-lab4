95498ae1454eb0f150061fc85a3f9c42122a2a24
LiamMAZIER <131826986+LiamMAZIER@users.noreply.github.com>
2025-09-17 15:48:34 +0200

    feat : user create now checks for duplicates

--- lab/src/controllers/user.js
+++ lab/src/controllers/user.js
@@ -3,21 +3,31 @@ const db = require('../dbClient')
 module.exports = {
   create: (user, callback) => {
     // Check parameters
-    if(!user.username)
+    if (!user.username) {
       return callback(new Error("Wrong user parameters"), null)
-    // Create User schema
+    }
+
+    // User schema
     const userObj = {
       firstname: user.firstname,
       lastname: user.lastname,
     }
-    // Save to DB
-    // TODO check if user already exists
-    db.hmset(user.username, userObj, (err, res) => {
+
+    // Check if user already exists
+    db.exists(user.username, (err, exists) => {
       if (err) return callback(err, null)
-      callback(null, res) // Return callback
+
+      if (exists) {
+        return callback(new Error("User already exists"), null)
+      }
+
+      // Save new user
+      db.hmset(user.username, userObj, (err, res) => {
+        if (err) return callback(err, null)
+        callback(null, res) // "OK"
+      })
     })
   },
-  // get: (username, callback) => {
-  //   // TODO create this method
-  // }
+
+  // get method still TODO
 }
