a38cff60a478df13e8d203277058fa086cc54569
Jules Havet <59026410+HuBac@users.noreply.github.com>
2025-09-17 15:50:12 +0200

    push

--- lab/src/controllers/user.js
+++ lab/src/controllers/user.js
@@ -17,7 +17,11 @@ module.exports = {
       callback(null, res) // Return callback
     })
   },
-  // get: (username, callback) => {
-  //   // TODO create this method
-  // }
+  get: (username, callback) => {
+    // Create User schema
+    db.get(username, (err, res) => {
+      if (err) return callback(err, null)
+      callback(null, res) // Return callback
+    })
+  },
 }
